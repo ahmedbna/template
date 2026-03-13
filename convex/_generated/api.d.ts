795defcef41b5dd4d14045aed653eaad4145099a
ahmedbna <hi@ahmedbna.com>
2026-03-13 17:05:34 +0200

    convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,10 +10,6 @@
 
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
-import type * as passwordReset from "../passwordReset.js";
-import type * as resendOTP from "../resendOTP.js";
-import type * as resendPasswordOTP from "../resendPasswordOTP.js";
-import type * as router from "../router.js";
 import type * as users from "../users.js";
 
 import type {
@@ -25,10 +21,6 @@ import type {
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   http: typeof http;
-  passwordReset: typeof passwordReset;
-  resendOTP: typeof resendOTP;
-  resendPasswordOTP: typeof resendPasswordOTP;
-  router: typeof router;
   users: typeof users;
 }>;
 
